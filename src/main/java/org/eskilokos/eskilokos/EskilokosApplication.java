02a6d6e2cc7b55c9b3ae4eeb4cc6c26fa74ded3c
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2026-05-29 23:40:23 -0600

    Primer Commit

---
+++ src/main/java/org/eskilokos/eskilokos/EskilokosApplication.java
@@ -0,0 +1,13 @@
+package org.eskilokos.eskilokos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskilokosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EskilokosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eskilokos/eskilokos/EskilokosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eskilokos.eskilokos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskilokosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
