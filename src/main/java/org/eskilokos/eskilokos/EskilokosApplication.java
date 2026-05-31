e073a36473a7b6fef527b2e528baadd9164f4fad
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2026-05-31 11:07:24 -0600

    Implementación de variables de entorno

--- src/main/java/org/eskilokos/eskilokos/EskilokosApplication.java
+++ src/main/java/org/eskilokos/eskilokos/EskilokosApplication.java
@@ -1,7 +1,9 @@
 package org.eskilokos.eskilokos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EskilokosApplication {
@@ -9,5 +11,5 @@ public class EskilokosApplication {
     public static void main(String[] args) {
         SpringApplication.run(EskilokosApplication.class, args);
     }
-
+    
 }
