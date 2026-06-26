dbc524cab1c12da3402f293a68af43df5b6fb45c
Arturo Rebolledo Dominguez <144817243+Melannuro@users.noreply.github.com>
2026-06-25 22:45:11 -0600

    Refactor CORS configuration to use CorsFilter

--- src/main/java/org/eskilokos/eskilokos/config/CorsConfig.java
+++ src/main/java/org/eskilokos/eskilokos/config/CorsConfig.java
@@ -1,18 +1,26 @@
 package org.eskilokos.eskilokos.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://frontendeskite.netlify.app/",
-                        "http://localhost:4200"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(true);
+        // Asegúrate de que las URLs no tengan espacios ocultos
+        config.addAllowedOrigin("https://frontendeskite.netlify.app");
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
